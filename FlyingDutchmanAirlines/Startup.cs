e2f80841ca676eb57b62ac786c8b245c5491b6ee
wittant <goldenmonstar@gmx.de>
2025-10-16 00:39:06 +0200

    first commit

---
+++ FlyingDutchmanAirlines/Startup.cs
@@ -0,0 +1,15 @@
+using Microsoft.AspNetCore.Builder;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace FlyingDutchmanAirlines;
+
+class Startup
+{
+    public void Configure(IApplicationBuilder app)
+    {
+        app.UseRouting();
+        app.UseEndpoints(endpoints => endpoints.MapControllers());
+    }
+
+    public void ConfigureServices(IServiceCollection services) => services.AddControllers();
+}
